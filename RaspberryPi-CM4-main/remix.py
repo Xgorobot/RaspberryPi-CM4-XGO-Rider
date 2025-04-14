5f3ad15c5ead35344e830971a6f73e7617391175
LuwuDynamics <hello@xgorobot.com>
2025-04-14 21:50:46 +0800

    RCM4V1

---
+++ RaspberryPi-CM4-main/remix.py
@@ -0,0 +1,13 @@
+from demos.uiutils import *
+import time
+lang=language()
+print(lang)
+if lang=='cn':
+	 print('lang = cn')
+	 os.system('sudo python3 main.py')
+	 
+else:
+     print('lang = en')
+     os.system('sudo python3 main.py')
+
+
